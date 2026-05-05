6776d4a143ab98fdd2d48160d5b09ac335376ad1
Mongoose24 <lucas.d.doyle@gmail.com>
2026-05-05 14:09:55 -0500

    yazi keybind patch

--- config/.config/yazi/plugins/smart-enter.yazi/main.lua
+++ config/.config/yazi/plugins/smart-enter.yazi/main.lua
@@ -5,7 +5,11 @@ local function setup(self, opts) self.open_multi = opts.open_multi end
 
 local function entry(self)
 	local h = cx.active.current.hovered
-	ya.emit(h and h.cha.is_dir and "enter" or "open", { hovered = not self.open_multi })
+	if h and h.cha.is_dir then
+		ya.emit("cd", { h.url })
+	else
+		ya.emit("open", { hovered = not self.open_multi })
+	end
 end
 
 return { entry = entry, setup = setup }
